7b548b7216c29e0451da0fdc933fb87e1ce5fc9f
DanilovNV <1995dnv@gmail.com>
2023-11-21 01:08:45 +0300

    Add project files.

---
+++ sqrt.cs
@@ -0,0 +1,27 @@
+﻿internal class sqrt
+{
+    public static double MethodSqrt(double x, double target)
+    {
+        double oldx;
+        do
+        {
+            oldx = x;
+            x = (x + target / x) / 2;
+        }
+        while (oldx != x);
+        Console.WriteLine("x= {0}", x);
+        Console.WriteLine("x= {0}", x * x);
+        return x;
+
+
+
+    }
+    private static void Main(string[] args)
+    {
+        double target = 2013;
+        double x = 1;
+        double result = MethodSqrt(x, target);
+        
+    }
+}
+    
